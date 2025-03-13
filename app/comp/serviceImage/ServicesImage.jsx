73da6d1b51dbcc252950b1c48ada0f97e1ad2b34
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-03-14 00:25:19 +0400

    fixed

--- app/comp/serviceImage/ServicesImage.jsx
+++ app/comp/serviceImage/ServicesImage.jsx
@@ -1,10 +1,26 @@
-
+import styles from "./serviceImage.module.css"
 
 
 export default function ServicesImage() {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className={styles.container}>
+                <div className={styles.layer}>
+                    <div className={styles.our_services_head_container}>
+                        <h1>
+                            OUR SERVICES
+                        </h1>
+                        <p>V&V handles your parcel delivery with the professionalism</p>
+                    </div>
+                </div>
+            </div>
+            <div
+                className={styles.our_services}
+            >
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </>
     );
 }
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,10 +1,10 @@
-
+import ServicesImage from "../comp/serviceImage/ServicesImage";
 
 
 export default function Services() {
     return (
         <div>
-
+            <ServicesImage />
         </div>
     );
 }
